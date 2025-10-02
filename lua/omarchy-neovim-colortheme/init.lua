7f040d934e6975014c940b8df0a87c4cd4c2dc17
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-10-02 16:01:16 +0300

    feat: make it actually working

--- lua/omarchy-neovim-colortheme/init.lua
+++ lua/omarchy-neovim-colortheme/init.lua
@@ -1,5 +1,4 @@
-local config_path = vim.fn.stdpath("config") .. "/lua/plugins/theme.lua"
-
+local config_path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
 local chunk = loadfile(config_path)
 if chunk then
   local config = chunk()
@@ -14,22 +13,19 @@ if chunk then
           plugin = item[1]
         end
 
-        if item.name == "LazyVim" and item.opts and item.opts.colorscheme then
+        if item[1] and item[1] == "LazyVim/LazyVim" and item.opts and item.opts.colorscheme then
           colorscheme_name = item.opts.colorscheme
         end
       end
     end
 
-    -- Install plugin
     if plugin then
-      vim.pack.add("https://github.com/" .. plugin)
+      vim.pack.add({"https://github.com/" .. plugin})
     end
 
     if colorscheme_name then
-      vim.defer_fn(function()
-        vim.cmd("colorscheme " .. colorscheme_name)
-        print("Colorscheme set to: " .. colorscheme_name)
-      end, 1000)
+      vim.cmd("colorscheme " .. colorscheme_name)
+      print("Colorscheme set to: " .. colorscheme_name)
     end
   end
 end
